1abbd6898a6e79fd0535a703c94a33600c75a0cc
Ole Skyggebjerg <ole@skyggebjerg.dk>
2025-03-28 10:31:24 +0100

    optimized

--- src/main.cpp
+++ src/main.cpp
@@ -20,49 +20,46 @@ MoToStepper stepper1( stepsPerRev, STEPDIR );  // create a stepper instance
 
 void setup() {
   pinMode(en_pin, OUTPUT);
-  pinMode(26, INPUT_PULLUP); // Optical fork for Y axis
-  pinMode(32, INPUT_PULLUP); // Signal from X axis MCU  
+  pinMode(26, INPUT_PULLUP); // signal from X axis MCU
+  pinMode(32, INPUT_PULLUP); // Optical fork for Y axis
   digitalWrite(en_pin, LOW); // enable driver
   stepper1.attach( stepPin, dirPin );
-  stepper1.setSpeed( 600 );              // 300 = 30 rev/min (if stepsPerRev is set correctly)
+  stepper1.setSpeed( 1200 );              // 300 = 30 rev/min (if stepsPerRev is set correctly)
   stepper1.setRampLen( stepsPerRev / 4); // Ramp length is 1/2 revolution
-  //stepper1.rotate(1);                    // start turning, 1=vorward, -1=backwards                    
-  //delay(10000);
-  //stepper1.stop();                       // stop turning
-  //stepper1.rotate(0);                    // stop turning
-  //delay(2000);
-  //stepper1.doSteps( stepsPerRev *4);        // do one revolution
-  //delay(10000);
-  //stepper1.doSteps( -stepsPerRev );
-
-  //delay(20000);
-  //digitalWrite(en_pin, HIGH); // disable driver
-
-
 }
 
 void loop() {
 
-  delay(1000); // wait for 1 second before checking the X axis signal
+  delay(2000); // wait for 1 second before checking the X axis signal
   while (digitalRead(26) == HIGH){ } // Wait until signal from X axis MCU goes LOW (X axis is in home position) 
 
 // --------------------------------******* Y AXIS ********--------------------------------
   if(digitalRead(32) == LOW){ // if the XY-stage is already in or beyond the home position (the optical fork is not blocked = LOW)
     stepper1.rotate(-1); //  start turning backwards, 1=vorward, -1=backwards
     while (digitalRead(32) == LOW){ } // keep moving until the optical fork is not blocked anymore
-      delay(1000); // wait for 1 second and let the stage move a bit more away from home position
       stepper1.stop(); // stop turning
+      stepper1.doSteps(-320); // move a bit more away
       delay(1000); // wait for 1 second
-      //stepper1.rotate(1); //  start turning forwards toward home position
     }
 
   if(digitalRead(32) == HIGH){
     stepper1.rotate(1); //  start turning forwards toward home position
-    while (digitalRead(32) == HIGH){ } // keep moving until the optical fork is not blocked
-  //if(digitalRead(32) == HIGH){ 
+    while (digitalRead(32) == HIGH){ } // keep moving until the optical fork is not blocked 
     stepper1.stop();
+    delay(100);
+
+    stepper1.doSteps(32); // move a bit more to the home position to make sure the optical fork is blocked (tolerance)
+    delay(1000);
+
+    stepper1.rotate(-1); //  start turning backwards, 1=vorward, -1=backwards
+    while (digitalRead(32) == LOW){ } // keep moving until the optical fork is blocked
+    stepper1.stop(); // stop turning
+    delay(100); // wait for 1 second
+
+    stepper1.doSteps(32); // move a bit more to the home position to make sure the optical fork is blocked (tolerance)
+    delay(1000);
+
     digitalWrite(en_pin, HIGH); // disable driver and release tourqe on the motor
     while (1){} // wait forever
-    //digitalWrite(en_pin, HIGH); // disable driver
   }
 }
